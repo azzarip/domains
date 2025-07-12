06ddb147d5cb96a1e0dec07151426be459a92641
Azzarip <paride21azzari@gmail.com>
2025-07-12 21:12:44 +0200

    removed old

--- stubs/view.blade.php
+++ stubs/view.blade.php
@@ -2,9 +2,9 @@
 
 @php
     $seo = new SEO(
-        title: 'Rum Expedition - Rum Verkostung in Zürich',
-        description: 'Rum Tasting on the 29. March 2025. Learn to taste Rum in a cruise through the Caribbean. Places are limited, book now.',
-        image: image('rum-expedition.webp'),
+        title: '',
+        description: '',
+        image: '',
     );
 @endphp
 
